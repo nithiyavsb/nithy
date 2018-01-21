fe8d6d5af75cfbcf80c4c687e3f32f4fbf864604
nithiyavsb <31506096+nithiyavsb@users.noreply.github.com>
2018-01-21 16:32:23 +0530

    Create armstorngtwo .c

---
+++ prime/armstorngtwo .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+int num,tem,sum=0,rem;
+printf("\n enter a number:\t");
+scanf("%d",& num);
+tem=num;
+while(num!=0)
+for (i=0;i<=num;i++)
+sum= sum+(rem*rem*rem);
+sum=(num/10,num/ 10);
+printf("\n%d it an armstorng intergal\n",tem);
+
+return 0;
+}
