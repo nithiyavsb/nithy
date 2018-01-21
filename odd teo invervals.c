aa4c4d1e1dab6fe71ede2c02b717c08e7712bd12
nithiyavsb <31506096+nithiyavsb@users.noreply.github.com>
2018-01-21 14:46:33 +0530

    Create odd teo invervals.c

---
+++ odd teo invervals.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int positive,negative;
+scanf(:%d%d;&positive,&negative);
+printf("enter the two (intervals):");
+printf("prime number b/w %d and %d:",positive,negative);
+while(positive<negative)
+for(i=2;i<=negative/2 ++i);
+if(negative%i==0)
+break;
+printf("%d",negative);
+++negative;
+return o;
+}
